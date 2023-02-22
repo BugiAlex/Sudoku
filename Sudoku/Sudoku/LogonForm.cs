013c4d5dded9f0f61fdeb705ea65c1bc48a39719
Buhaitsov Oleksii <bugajcovalexej@gmail.com>
2023-02-22 15:24:40 +0200

    Add Form Registration

--- Sudoku/Sudoku/LogonForm.cs
+++ Sudoku/Sudoku/LogonForm.cs
@@ -4,14 +4,7 @@ namespace Sudoku
     {
         public LogonForm()
         {
-            InitializeComponent();
-            this.Text = "1234";
-            Label label1 = new Label();
-            label1.Text = "First text in project";
-            label1.Location = new Point(20, 20);
-            label1.AutoSize = true;
-            Controls.Add(label1);
-
+            InitializeComponent();            
         }
     }
 }
